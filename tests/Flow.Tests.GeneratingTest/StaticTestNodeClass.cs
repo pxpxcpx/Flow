e2c4fb539ed1418bb1515bdeab255d9fc39d066f
Pixel <60724881+pxpxcpx@users.noreply.github.com>
2025-11-29 00:00:52 +0800

    feat: 统一生成代码风格, 新建专用生成测试项目

---
+++ tests/Flow.Tests.GeneratingTest/StaticTestNodeClass.cs
@@ -0,0 +1,15 @@
+﻿using Flow.SDK.Plugins.Attributes;
+
+namespace Flow.Tests.GenerationTest;
+
+public partial class StaticTestNode
+{
+    [StaticNode("Test Node", "Node for Add method")]
+    public static int TestMethod(
+        [Input("", "")]int a,
+        [Input("1", "")]int b1,
+        [Input("3", "")]int c)
+    {
+        return a + c;
+    }
+}
